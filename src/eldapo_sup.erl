8b4d93e8af19b683c927be5ae9c53aa98eed6fc7
yves <steve.harrison@gmx.net>
2012-09-09 23:51:31 +0200

    adapt change from acceptor module and correct supervisor shutdown method

--- src/eldapo_sup.erl
+++ src/eldapo_sup.erl
@@ -12,10 +12,6 @@
 %% Helper macro for declaring children of supervisor
 % -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
-
--define(GEN_LISTNER_TCP_HANDLER, eldapo_handler).
--define(REG_NAME, eldapo).
-
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -32,20 +28,13 @@ init([]) ->
     {ok, { {one_for_one, 5, 10},
             [
                 % first child tcp listner
-                { gen_listener_tcp,
-                    { gen_listener_tcp, start_link,
-                        % options for tcp listner
-                        [
-                            {local, ?REG_NAME},
-                            ?GEN_LISTNER_TCP_HANDLER,
-                            [], []
-                        ]
-                    },
-                    permanent, brutal_kill, worker, [] },
+                { eldapo_tcp_acceptor,
+                    {eldapo_tcp_acceptor, start_link, []},
+                    permanent, brutal_kill, worker, []},
                 % second child eldapo handler supervisor
                 { eldapo_handler_sup,
                     {eldapo_handler_sup,start_link, []},
-                    permanent, brutal_kill, supervisor, [] }
+                    permanent, infinity, supervisor, []}
             ]
         }}.
 
